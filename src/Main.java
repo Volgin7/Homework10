edd9fb209b97da3eba09de87a65053b28a31715c
Volgin7 <8612020@gmail.com>
2023-03-13 15:19:59 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        task1();
 
     } // end of main
+
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    } // end of task
 
 } // end of Main
