7bc43c4780cf442a96d7793879e8ec3bc731a828
Volgin7 <8612020@gmail.com>
2023-03-13 15:30:27 +0300

    +task2 & task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         task1();
+        task2();
+        task3();
 
     } // end of main
 
@@ -14,6 +16,27 @@ public class Main {
 
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println();
+    } // end of task
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        System.out.println();
+    } // end of task
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        String fullName = "Иванов Семён Семёнович";
+
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+        System.out.println();
     } // end of task
 
 } // end of Main
